b99a22a43fc3c3b23798cb9c81f9b0844ecbaeed
monosniper <monosniper@gmail.com>
2024-01-28 18:00:18 +0500

    dasdas

--- controllers/setting-controller.js
+++ controllers/setting-controller.js
@@ -13,9 +13,13 @@ class SettingController {
         return res.json({data})
     }
     async getOnline(req, res, next) {
-        const result = await util.queryBasic(process.env.MINECRAFT_HOST, parseInt(process.env.MINECRAFT_PORT), { enableSRV: true })
+        try {
+            const result = await util.queryBasic(process.env.MINECRAFT_HOST, parseInt(process.env.MINECRAFT_PORT), { enableSRV: true })
 
-        return res.json({data: result.players})
+            return res.json({data: result.players})
+        } catch (e) {
+            return res.json({data: false})
+        }
     }
 }
 
